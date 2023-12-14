a7851c0fc6e183ed667fbb58736dd395aeb28cf5
Ian Gabriel Pereira de Queiroz <iangabrielpq392@gmail.com>
2023-12-14 00:08:27 -0300

    Terceiro commit

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ $('document').ready(function() {
     $('form').on('submit', function(e) {
         e.preventDefault()
         const valorTarefa = $('#tarefa').val()
-        const novoItem = $(`<li></li>`)
+        const novoItem = $('<li></li>')
         $(`<p>${valorTarefa}<p>`).appendTo(novoItem)
         $(novoItem).appendTo('ul')
         $('#tarefa').val('')
 
-    $('ul li').click(function() {
-        $('<li text-decoration: line-through ></li>').appendTo(novoItem)
     })
+
+    $('ul li').on("click", function() {
+        $(this).addClass("ativo")
     })
+    
 })
